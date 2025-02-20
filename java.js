ec6d094b9fcfafd6d02a47731c81a09535741e2a
Gintra121 <sopheaktravannak04@gmail.com>
2025-02-21 00:03:45 +0700

    push4

--- java.js
+++ java.js
@@ -1,33 +1,54 @@
 $(document).ready(function(){
-    $('.click').click(function(){
-        $(this).toggleClass('.clicked');
-        $('.navblock').toggleClass('nav-toggle');
-    });
-    $(window).on('scroll', function(){
-        $('.click').removeClass('.clicked');
-        $('.navblock').removeClass('nav-toggle');
-        if($(window).scrollTop()>30){
-            $('header').addClass('header-online');
-        } else{
-            $('header').removeClass('header-online');
-        }
-    });
+  $('.click').click(function(){
+      $(this).toggleClass('.clicked');
+      $('.navblock').toggleClass('nav-toggle');
+  });
+  $(window).on('scroll', function(){
+      $('.click').removeClass('.clicked');
+      $('.navblock').removeClass('nav-toggle');
+      if($(window).scrollTop()>30){
+          $('header').addClass('header-online');
+      } else{
+          $('header').removeClass('header-online');
+      }
+  });
 
 });
-document.addEventListener('DOMContentLoaded', function () {
-    const blocks = document.querySelectorAll('.box-wrap .block');
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            } else {
-                entry.target.classList.remove('visible');
-            }
-        });
-    });
+// for disappeared and appera when scroll
+document.addEventListener("DOMContentLoaded", function () {
+  const blocks = document.querySelectorAll(".box-wrap .block");
 
-    blocks.forEach(block => {
-        observer.observe(block);
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
     });
+  });
+
+  blocks.forEach((block) => {
+    observer.observe(block);
+  });
 });
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+      header.classList.add('sticky');
+  } else {
+      header.classList.remove('sticky');
+  }
+});
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
